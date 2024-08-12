24744dc11ed7338d14e107a8934a3dc39cc3da80
richie1988 <richiessikaonga@gmail.com>
2024-08-12 17:16:25 +0200

    Error handling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
                 <li><Link to='hero' smooth={true} offset={0} duration={500}>Home</Link></li>
                 <li><Link to='programs' smooth={true} offset={-260} duration={500}>Programs</Link></li>
                 <li><Link to='about' smooth={true} offset={-150} duration={500}>About us</Link></li>
-                <li><Link to='campaign' smooth={true} offset={-260} duration={500}>Campaign</Link></li>
+                <li><Link to='campagn' smooth={true} offset={-260} duration={500}>Campaign</Link></li>
                 <li><Link to='teamMembers' smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
                 <li><Link to='contact' smooth={true} offset={-260} duration={500} className="btn">Contact</Link></li>
             </ul>
